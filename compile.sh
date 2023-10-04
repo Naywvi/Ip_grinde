2ef63d2c296abe3b781d54fc7cdd37bf656c4991
Naywvi <nagib.lakhdari.pro@gmail.com>
2023-10-05 01:47:07 +0200

    gtk

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@
 read -n 1 -s -r -p "Press any key to continue compilation"
 
 gcc -I/usr/include/gtk-3.0 -o main main.c `pkg-config --cflags --libs gtk+-3.0`
+clear
+echo " => done press ./main to run"
